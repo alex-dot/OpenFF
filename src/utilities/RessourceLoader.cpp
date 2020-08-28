0554270c96b2bade39c09576d54d5e0bc4578553
Alexander Herr <thissideup@riseup.net>
2020-08-28 19:55:02 +0200

    RessourceLoader now using TerminalOutput

--- src/utilities/RessourceLoader.cpp
+++ src/utilities/RessourceLoader.cpp
@@ -6,13 +6,14 @@ namespace OpenFF {
 RessourceLoader::RessourceLoader() {
   _manager = new PluginManager::Manager<Trade::AbstractImporter>;
   _png_importer = _manager->loadAndInstantiate("PngImporter");
-  if(!(_png_importer)) std::exit(1);
+  if(!(_png_importer)) FatlIOError("Could not load PngImporter Plugin");
 }
 
 void RessourceLoader::getImage(
         std::string location,
         Containers::Optional<Trade::ImageData2D>& image) {
-  if(!_png_importer->openFile(location)) std::exit(2);
+  if(!_png_importer->openFile(location))
+    FatlIOError("Could not load Image at location: "+location);
   image = _png_importer->image2D(0);
   CORRADE_INTERNAL_ASSERT(image);
 }
